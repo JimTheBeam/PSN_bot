f5aad937e3cb826572e45ca17d294ed2b2c1a815
JimTheBeam <24dmitriy@mail.ru>
2020-11-14 04:51:36 +0300

    add old_price, image_link in parser

--- parsers/utils.py
+++ parsers/utils.py
@@ -10,12 +10,33 @@ def get_html(url, headers=None, params=None):
 
 
 def get_current_price(item):
+    """get current price from part of html document
+    return None if not found"""
     price_tag = item.find('span', class_='content__game_card__price__current')
     if price_tag is None:
         return None
     else:
         return price_tag.get_text().replace('\xa0', '')
 
+
+def get_plus_price(item):
+    """get ps plus price from part of html document
+    return None if not found"""
+    plus_price_tag = item.find('span', class_='content__game_card__price_plus')
+    if plus_price_tag is None:
+        return None
+    else:
+        return plus_price_tag.get_text().strip().replace('\xa0', '')
+
+
+def get_old_price(item):
+    """get old price (if the game has a discount) from part of html document
+    return None if not found"""
+    old_price_tag = item.find('span', class_='old_price')
+    if old_price_tag is None:
+        return None
+    else:
+        return old_price_tag.get_text().replace('\xa0', '')
 
 
 def get_content(html):
@@ -26,8 +47,10 @@ def get_content(html):
     for item in items:
         games.append({
             'title': item.find('span', class_='title').get_text(),
-            'current_price': get_current_price(item)
-            # 'ps_plus_price': item.find('span', class_='content__game_card__price_plus').get_text() if not None else None
+            'current_price': get_current_price(item),
+            'plus_price': get_plus_price(item),
+            'old_price': get_old_price(item),
+            'image_link': item.find('img', class_='game-card--image loaded')
         })
 
     pprint(games)
