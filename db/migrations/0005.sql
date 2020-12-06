ee61331435ae62cb6fc874a3a71297cf273cfef7
JimTheBeam <24dmitriy@mail.ru>
2020-12-06 22:10:06 +0300

    migration 0005. change user_id to chat_id as pk in table users and in table user_games

---
+++ db/migrations/0005.sql
@@ -0,0 +1,24 @@
+-- change table user_games. 
+DROP table users;
+
+
+CREATE TABLE users (
+    id BIGSERIAL,
+    chat_id INTEGER PRIMARY KEY NOT NULL UNIQUE,        
+    created_time TIMESTAMP);
+
+
+DROP TABLE user_games;
+
+
+CREATE TABLE user_games (
+    chat_id INTEGER REFERENCES users ON DELETE CASCADE,
+    game_id INT REFERENCES games ON DELETE CASCADE,
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    game_price  INTEGER,
+    game_plus_price INTEGER,
+    CONSTRAINT user_games_primary_key PRIMARY KEY (chat_id, game_id)    
+);
+
+
+UPDATE migration_version SET version = 5;
